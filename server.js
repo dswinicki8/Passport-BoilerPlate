1c8de9b207e32f4bcd9c18a8f4b4937673496693
Drew Swinicki <dswinicki93@gmail.com>
2020-08-01 11:00:08 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import Cors from 'cors';
+import bodyPareser from 'body-parser';
+import logger from 'morgan';
+import passport from 'passport';
+const app = express();
+
+const API_PORT = process.env.API_PORT || 3000;
+
+require('.config/passport');
+
+app.use(Cors());
+app.use(bodyPareser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(passport.initialize());
+
+require('./routes.loginUser')(app);
+require('./routes.registerUser')(app);
+require('./routes.findUsers')(app);
+require('./routes.deleteUser')(app);
+require('./routes.updateUser')(app);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+
+module.export = app;
